8eb9a8534e9bc12d84844dd4a77afae5c24d7661
J7st2k <sav-n-an@mail.ru>
2024-03-08 22:17:59 +0700

    2.8 done, redacted, comms

--- mainwindow.h
+++ mainwindow.h
@@ -1,23 +1,32 @@
 #ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#define win_h
+#include <QFrame>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLayout>
 
-#include <QMainWindow>
-
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
+class Counter:public QLineEdit
 {
     Q_OBJECT
-
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    Counter(const QString & contents, QWidget *parent=0):
+        QLineEdit(contents,parent){}
+signals:
+    void tick_signal();
+public slots:
+    void add_one();
+};
 
-private:
-    Ui::MainWindow *ui;
+class Win: public QWidget
+{
+    Q_OBJECT
+protected:
+    QLabel *label1,*label2;
+    Counter *edit1,*edit2;
+    QPushButton *calcbutton;
+    QPushButton *exitbutton;
+public:
+    Win(QWidget *parent = 0);
 };
 #endif // MAINWINDOW_H
